e3b614b21ab87e5e6174571203b5b515673018ef
Ajeet <ajeet@now.bike>
2022-09-29 09:43:53 +0530

    added OTPgenerator function

--- src/controllers/otp/otp.controller.js
+++ src/controllers/otp/otp.controller.js
@@ -1,11 +1,5 @@
 const {Otp} = require('../../models'); 
-const otpGenerator = require('otp-generator');
 const AWS = require('aws-sdk');
-const fast2sms = require('fast-two-sms');
-
-
-
-
 
 AWS.config.loadFromPath(__dirname +'/../../config/aws/config.json');
 
@@ -18,7 +12,7 @@ export const generateOtp = async (req, res) => {
             const response = { status: 'Failure', Details: 'Phone Number Not Provided' }
             return res.statut(400).send(response);
         }
-        const otp = otpGenerator.generate(4, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false });
+        const otp = otpGenerator()
         const now = new Date();
         const expiration_time = new Date(now.getTime() + 2 * 60000);
         const otp_instance = await Otp.create({
@@ -35,37 +29,27 @@ export const generateOtp = async (req, res) => {
             Message: phone_message,
             PhoneNumber:phone_number
         }
-        
-        var options = {
-            authorization : "8IhgxB2tp5ZrSUfzn9W6LAC07jXyuFRTmGDqvJ4KbadeE1MO3Ng3rlAnoScD8jWpfFTudLqZwCe61tP9",
-            message : ' This is a test OTP Code Message',
-            numbers : ['7376235783']
-        } ;
-        // send this message
-        fast2sms.sendMessage(options)
-            .then((response) => {
-                console.log("f2sms result", response)
-                // return res.status(200).send(response)
-            })
-            .catch((err) => {
-                console.log("f2sms err", err);
-                // return res.status(400).send(err)
-            })
-
-            return res.status(200).send('sms sent')
-        // var publishTextPromise = new AWS.SNS({ apiVersion: '2012-11-05' }).publish(params).promise();
-        // publishTextPromise.then((data) => {
-        //     console.log("message",data)
-        //     return res.status(200).send({"status":"Success","Details":"Otp Send to SNS"})
-        // }).catch((err) => {
-        //     return res.send({status:"Failure",Details:err})
-        // })
+        var publishTextPromise = new AWS.SNS({ apiVersion: '2012-11-05' }).publish(params).promise();
+        publishTextPromise.then((data) => {
+            console.log("message",data)
+            return res.status(200).send({"status":"Success","Details":"Otp Send to SNS"})
+        }).catch((err) => {
+            return res.send({status:"Failure",Details:err})
+        })
     } catch (error) {
         const response = { status: "Failure", Details: error.message }
         return res.status(400).send(response);
     }
 };
 
+function otpGenerator(){
+    var digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < 4; i++ ) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
+}
 
 export const verifyOtp = async (req, res) => {
     try {
