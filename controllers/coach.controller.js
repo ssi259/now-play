4bfac21604f052b6991cdd6333318d17a82e8409
Ajeet <ajeet@now.bike>
2022-10-03 18:56:16 +0530

    coach register api

--- controllers/coach.controller.js
+++ controllers/coach.controller.js
@@ -1,11 +1,43 @@
 import {Coach} from '../models'
 
 export const registerCoach = async (req, res) => {
-
-    
-    return res.status(200).send("Coach Register API Called");
+    try {
+        const {
+          name, contactNumber, email, address, status, sportsName, experience, verified, tier, awards, teamAffiliations,about
+        } = req.body;
+        const coach = await Coach.findOne({
+          where: { contactNumber },
+        });
+        if (coach) {
+          throw new Error('User already exists with same email');
+        }
+        const payload = {
+            name,
+            contactNumber,
+            email,
+            address,
+            status,
+            sportsName,
+            experience,
+            verified,
+            tier,
+            awards,
+            teamAffiliations,
+            about
+          }
+        const newCoach = await Coach.create(payload);
+        return res.status(200).send({coach:newCoach})
+      } catch (error) {
+        return res.status(400).send(error.message);
+    }
 }
 
 export const getCoaches = async (req, res) => {
-    return res.status(200).send(" get Coaches List")
+    try {
+        const coaches = await Coach.findAll();
+        return res.status(200).send({coachesList:coaches})
+        
+    } catch (err) {
+        return res.status(400).send(error.message);
+    }
 }
