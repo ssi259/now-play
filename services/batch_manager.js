9b0bdaf611089248e66647452479546aa887fbd4
Arjun <arjun.verma8412@gmail.com>
2022-10-04 03:59:08 +0530

    optimised+db-config-read-update

--- services/batch_manager.js
+++ services/batch_manager.js
@@ -1,4 +1,15 @@
-const { Sequelize } = require("sequelize")
+const dbConfig = require("../config/db_config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min
+  }
+});
 
 exports.pre_process_params = async(req,resp)=>{
     const results =await sequelize.query("SELECT batches.id , batches.price ,batches.start_time, batches.end_time,batches.days , arena.name AS arena_name, coaches.name AS coach_name , coaches.experience, coaches.rating, sports.name,sports.type, (6371 *acos(cos(radians("+ req.query.lat + ")) * cos(radians(lat)) * cos(radians(lng) - radians("+req.query.lng+")) + sin(radians("+req.query.lat+")) * sin(radians(lat)))) AS distance FROM (((Batches batches INNER JOIN Arenas arena on batches.id = arena.id) INNER JOIN Coaches coaches ON batches.coach_id = coaches.id) INNER JOIN Sports sports ON batches.sports_id = sports.id) HAVING distance < 100000   ORDER BY distance LIMIT 0, 20;",{ type: Sequelize.QueryTypes.SELECT }).then((result)=>{
