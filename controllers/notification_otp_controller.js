c5f58b6a2209ad2ac7261199a8d86613756c9402
Ajeet <ajeet@now.bike>
2022-10-10 15:05:07 +0530

    otp revisited

---
+++ controllers/notification_otp_controller.js
@@ -0,0 +1,129 @@
+const jwt =  require('jsonwebtoken')
+const { User, Notification } = require('../models'); 
+const AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('../config/aws_config.json');
+
+
+exports.generateOtp = async (req, res) => {
+    try {
+        const { phoneNumber } = req.body;
+        if (!phoneNumber) {
+            const response = { status: 'Failure', Details: 'Phone Number Not Provided' }
+            return res.status(400).send(response);
+        }
+        const now = new Date();
+        let otp;
+        const expirationDate = new Date(now.getTime() + 10 * 60000);
+        const otpInstance = await Notification.findOne({
+            where: {
+                phoneNumber: phoneNumber,
+                notificationType: "otp",
+                expirationDate: {
+                    [op.gt]:now
+                }
+            }
+        });
+        if (!otpInstance) {
+            otp = otpGenerator();
+            const  OtpObj = await Notification.create({
+                otp: otp,
+                expirationDate: expirationDate,
+                isVerified: false,
+                phoneNumber: phoneNumber,
+                channelType: "sms",
+                notificationType: "otp"
+            })
+        }
+        else {
+            otp=otpInstance.otp
+        }
+        const phoneMessage =
+            `Dear User,\n`
+            + `${otp} is your OTP for Phone Number Verfication. Please enter the OTP to verify your phone number`
+        const params = {
+            Message: phoneMessage,
+            PhoneNumber:phoneNumber
+        }
+        var publishTextPromise = new AWS.SNS({ apiVersion: '2010-03-31' }).publish(params).promise();
+        publishTextPromise.then((data) => {
+            return res.status(200).send({"status":"Success","Details":"OTP Sent Successfully"})
+        }).catch((err) => {
+            return res.send({status:"Failure",Details:err})
+        })
+    } catch (error) {
+        const response = { status: "Failure", Details: error.message }
+        return res.status(400).send(response);
+    }
+};
+
+exports.verifyOtp = async (req, res) => {
+    try {
+        const { phoneNumber, otp } = req.body;
+        const currentDate = new Date();
+        if (!phoneNumber) {
+            const response = { "Status": "Failure", "Details": "Phone Number Not Provided" }
+            return res.status(400).send(response)
+        }
+        if (!otp) {
+            const response = { "Status": "Failure", "Details": "OTP not Provided" }
+            return res.status(400).send(response);
+        }
+        const otpInstance = await Notification.findOne({
+            where: 
+            {
+                otp: otp,
+                notificationType: "otp",
+            }
+        });
+        if (otpInstance != null) {
+            if (!otpInstance.isVerified) {
+                const response={"Status":"Failure","Details":"OTP Already Used"}
+                return res.status(400).send(response);
+            }
+            if (otpInstance.expirationDate > currentDate) {
+                const response={"Status":"Failure","Details":"OTP Expired"}
+                return res.status(400).send(response);
+            }
+            if (otpInstance.otp != otp) {
+                const response={"Status":"Failure","Details":"OTP Not Matched"}
+                return res.status(400).send(response);
+            }
+            otpInstance.isVerified = true
+            var otpUpdatedInstance = await otpInstance.save();
+            const userInstance = await User.create({
+                phoneNumber: phoneNumber,
+                isMobileVerified: true,
+            });
+            const token = jwt.sign(
+                {
+                    user: {
+                    userId: userInstance.id,
+                    phoneNumber: userInstance.phoneNumber,
+                    createdAt: new Date(),
+                    },
+                },
+                process.env.JWT_SECRET_KEY,
+            );
+            userInstance.verifyToken = token;
+            var userUpdatedInstance = await userInstance.save();
+            const response = { "Status": "Success", "Details": {user:userUpdatedInstance, verifyToken:token} }
+            return res.status(200).send(response);   
+        } else {
+            const response={"Status":"Failure","Details":"Bad Request"}
+            return res.status(400).send(response);
+        }
+    } catch (error) {
+        const response = { status: "Failure", Details: error.message }
+        return res.status(400).send(response);
+    }
+}
+
+function otpGenerator(){
+    var digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < 4; i++ ) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
+}
--- routes/otp.js
+++ routes/otp.js
@@ -1,4 +1,4 @@
-const otpController = require('../controllers/otp.controller');
+const otpController = require('../controllers/notification_otp_controller');
 const express = require('express');
 
 const routes = express.Router();
