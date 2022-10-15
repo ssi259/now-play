abc80d3da811f4dd83c76f747e4fb7eb97229750
vinayakkakkar <vinayakkakkar17@gmail.com>
2022-10-15 15:26:29 +0530

    sports_api

--- controllers/sports_controller.js
+++ controllers/sports_controller.js
@@ -5,7 +5,7 @@ exports.create_sport = async(req,resp)=>{
     try{
         var input_response =  await SportManager.pre_process_create_sport(req,resp)
         var processed_reponse =  await SportManager.process_sport_input_req(input_response)
-        var post_process_response = await SportManager.post_create_sport_process(req,resp,processed_reponse)
+        var post_process_response = await SportManager.post_sport_process(req,resp,processed_reponse)
     }catch(e){
         console.log(e)
     }finally{
