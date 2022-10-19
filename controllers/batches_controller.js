8ab79be2f86198030d182a4a4468b26b0440d73c
vinayakkakkar <vinayakkakkar17@gmail.com>
2022-10-19 21:17:00 +0530

    batch_details_api

--- controllers/batches_controller.js
+++ controllers/batches_controller.js
@@ -22,3 +22,14 @@ exports.create_batch = async(req,resp) =>{
     }finally{
     }
 }
+exports.batch_details = async(req,resp) =>{
+
+    try{
+        var input_response = await BatchManager.pre_process_batch_details(req,resp)
+        var processed_reponse =  await BatchManager.process_batch_details_input_req(input_response)
+        var post_process_response = await BatchManager.post_process(req,resp,processed_reponse)
+    }catch(e){
+        console.log(e)
+    }finally{
+    }
+}
--- routes/batches.js
+++ routes/batches.js
@@ -6,5 +6,6 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/search', batch_controller.search_batch);
 router.post('/', batch_controller.create_batch);
+router.get('/:id',batch_controller.batch_details);
 module.exports = router;
 
