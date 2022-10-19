ce5cf15d5d1472961e0955599bd3e1d7341d93c6
vinayakkakkar <vinayakkakkar17@gmail.com>
2022-10-19 20:27:58 +0530

    get_sports_api

--- routes/sports.js
+++ routes/sports.js
@@ -5,5 +5,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', sport_controller.create_sport);
+router.get('/',sport_controller.sports_list);
 module.exports = router;
 
--- services/sport_manager.js
+++ services/sport_manager.js
@@ -16,4 +16,21 @@ exports.process_sport_input_req = async(input_response)=>{
 exports.post_sport_process = async(req,resp,input_response)=>{
   resp.send(input_response)
 }
+exports.post_create_sport_process = async(req,resp,input_response)=>{
+    resp.send(input_response)
+}
 
+exports.pre_process_sports_list = async(req,resp)=>{
+  const sports_list = await models.Sports.findAll();
+      if (sports_list) {
+          return sports_list
+      } else {
+          resp.status(400).send('list not found');
+      }
+}   
+exports.process_sports_list_input_req = async(input_response)=>{
+  return input_response
+}
+exports.post_sports_list_process = async(req,resp,input_response)=>{
+  resp.send(input_response)
+}
