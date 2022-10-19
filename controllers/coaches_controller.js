b19f4d4acdd9b542d536b8a71fd89fcfaa04758d
Ajeet <ajeet@now.bike>
2022-10-19 14:54:08 +0530

    update coach manager and controller code

--- controllers/coaches_controller.js
+++ controllers/coaches_controller.js
@@ -2,9 +2,8 @@ const coachManager = require('../services/coach_manager');
 
 exports.createCoach = async (req, resp) => {
     try {
-        const coach = await coachManager.process_create_coach(req, resp);
-        return resp.status(200).send({status:"Success",coach:coach})
+        var process_response = await coachManager.process_create_coach(req, resp);
     } catch (e) {
-        return resp.status(400).send({"status":"Failure","Details":e.message})
+        resp.status(500).send({status:"Failure",details:e.message})
     }
 }
