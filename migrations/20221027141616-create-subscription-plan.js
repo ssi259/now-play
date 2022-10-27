af978dbbf4fce1a1f6cb7198abec177037dd2a50
Ajeet <101518396+ajeet-now@users.noreply.github.com>
2022-10-27 19:58:36 +0530

    feat:subscription plan model create (#54)
    * subscription plan model created
    
    * change batchId to batch_id in sub plan model
    
    * updated batchId to batch_id in subscriptionplan table

---
+++ migrations/20221027141616-create-subscription-plan.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('SubscriptionPlans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      batch_id: {
+        type: Sequelize.INTEGER
+      },
+      plan_name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      tag: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('SubscriptionPlans');
+  }
+};
---
+++ models/subscriptionplan.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SubscriptionPlan extends Model {
+    static associate(models) {
+    }
+  }
+  SubscriptionPlan.init({
+    batch_id: DataTypes.INTEGER,
+    plan_name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    status: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    tag: DataTypes.STRING,
+    type: DataTypes.STRING,
+    duration: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'SubscriptionPlan',
+  });
+  return SubscriptionPlan;
+};
