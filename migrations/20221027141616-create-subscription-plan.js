f2a66470173229d3b63f38367bc475645e64039b
Ajeet <ajeet@now.bike>
2022-10-27 19:52:02 +0530

    updated batchId to batch_id in subscriptionplan table

--- migrations/20221027141616-create-subscription-plan.js
+++ migrations/20221027141616-create-subscription-plan.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      batchId: {
+      batch_id: {
         type: Sequelize.INTEGER
       },
       plan_name: {
