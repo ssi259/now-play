f4390c1e1b0a4d2dad2f2327e16f553d477a2ec8
vinayakkakkar <vinayakkakkar17@gmail.com>
2022-10-31 13:52:02 +0530

    enrollment_controller

---
+++ controllers/enrollments_controller.js
@@ -0,0 +1,1 @@
+const EnrollmentManager = require("../services/enrollment_manager")
---
+++ routes/enrollment.js
@@ -0,0 +1,3 @@
+const enrollment_controller = require("../controllers/enrollments_controller.js")
+var express = require('express');
+var router = express.Router();
