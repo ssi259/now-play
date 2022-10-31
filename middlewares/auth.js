042bf076e7efa3e479a92b2b4281e17e5d1fdf4e
Ajeet <ajeetsing187@gmail.com>
2022-11-01 00:41:16 +0530

    add auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+exports.auth = async (req, resp, next) => {
+    const jwt_token = req.headers['x-access-token'];
+    if (!jwt_token) {
+        return resp.status(401).send({status:"failure",message:'token not found'})
+    }
+    try {
+        const decoded = jwt.verify(jwt_token, process.env.JWT_SECRET_KEY)
+        req.user = decoded
+    } catch(e) {
+        return resp.status(401).send({status:"failure",message:'invalid token'})
+    }
+    next()
+}
