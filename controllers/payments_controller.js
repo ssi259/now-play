6fba091c903643894e75f5def2f531b16315a017
vinayakkakkar <vinayakkakkar17@gmail.com>
2022-11-01 21:00:11 +0530

    update_payment

--- controllers/payments_controller.js
+++ controllers/payments_controller.js
@@ -14,3 +14,15 @@ exports.create_payment = async(req,resp)=>{
     }finally{
     }
 }
+exports.update_payment = async(req,resp) =>{
+
+    try{
+        var input_response = await PaymentManager.pre_process_update_payment(req,resp)
+        var processed_reponse =  await PaymentManager.process_update_payment_input_req(input_response)
+        var post_process_response = await PaymentManager.post_process_update_payment(req,resp,processed_reponse)
+    }catch(e){
+        const status_code = e.statusCode ? e.statusCode : 500
+        return resp.status(status_code).send({ status: "Failure", message: e.name })
+    }finally{
+    }
+}
