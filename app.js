3114284d79071eea3dc6b9fce02c39dbf572ea65
ssi259 <shivendraps230@gmail.com>
2022-11-03 02:43:57 +0530

    add_cors (#73)
    Co-authored-by: Arjun <arjun.verma8412@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fileUpload = require("express-fileupload");
@@ -19,6 +20,7 @@ var planRouter = require('./routes/plans')
 
 var app = express();
 app.use(fileUpload());
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
