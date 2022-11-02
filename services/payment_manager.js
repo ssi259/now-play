154334de845d43a732b740ada2ed2a90713cb2a8
vinayakkakkar <vinayakkakkar17@gmail.com>
2022-11-02 21:01:14 +0530

    update_payment:

--- services/payment_manager.js
+++ services/payment_manager.js
@@ -1,5 +1,6 @@
 const models = require("../models");
 const Api500Error = require('../error/api500Error')
+const Api400Error = require('../error/api400Error')
 
 
 exports.pre_process_create_payment = async(req,resp)=>{
@@ -15,5 +16,20 @@ exports.process_payment_input_req = async(input_response)=>{
   return input_response
 }
 exports.post_payment_process = async(req,resp,input_response)=>{
+  resp.send(input_response)
+}
+exports.pre_process_update_payment = async(req,resp)=>{
+  const update_payment = await  models.Payment.update(req.body, {where: {id:req.params.id}})
+  .then(() => {return models.Payment.findOne({where: {id:req.params.id}})})
+  if (update_payment) {
+      return update_academy
+  } else {
+    throw new Api400Error(`Error In Updating payment`)
+  }
+}
+exports.process_update_payment_input_req = async(input_response)=>{
+  return input_response
+}
+exports.post_process_update_payment = async(req,resp,input_response)=>{
   resp.send(input_response)
 }
