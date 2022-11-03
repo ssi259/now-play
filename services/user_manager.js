1d0488502c8ccc3ff6ffacb18c13d1ffe2b5c500
Ajeet <101518396+ajeet-now@users.noreply.github.com>
2022-11-04 00:57:06 +0530

    feat :user update by id (#71)
    * user get by user_id api
    
    * user update by id api
    
    Co-authored-by: Ajeet <ajeetsing187@gmail.com>

--- services/user_manager.js
+++ services/user_manager.js
@@ -16,3 +16,24 @@ exports.process_get_user_by_id = async (user_id) => {
 exports.post_process_get_user_by_id = async (user, resp) => {
     resp.status(200).send({status:"success",message:"user retrieved successfully",data:user})
 }
+
+
+exports.pre_process_update_user_by_id = async (req) => {
+    return {user_id:req.params.id , data:req.body}
+}
+
+exports.process_update_user_by_id = async (input_data, req) => {
+    const { user_id, data } = input_data
+    const [affected_rows] = await models.User.update(data, {
+        where: {
+          id:user_id
+        }
+    })
+    if (!affected_rows) {
+        throw new Api400Error("invalid request")
+    }
+}
+
+exports.post_process_update_user_by_id = async (resp) => {
+    resp.status(200).send({status:"success",message:"user updated successfully"})
+}
