fe83a28b4dd28649bebeb8cf68e1f87f789462ed
ajeet-now <ajeet@now.bike>
2022-11-04 22:19:56 +0530

    slight change

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 exports.auth = async (req, resp, next) => {
     const jwt_token = req.headers['x-access-token'];
     if (!jwt_token) {
-        return resp.status(401).send({status:"failure",message:'token not found'})
+        return resp.status(401).send({ status:"failure",message:'token not found' })
     }
     try {
         const decoded = jwt.verify(jwt_token, process.env.JWT_SECRET_KEY)
