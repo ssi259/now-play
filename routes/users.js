0f53a4a531b945ec57fb877b2bdf342d22b7dc02
ajeet-now <ajeet@now.bike>
2022-11-08 19:48:38 +0530

    add auth in users routes

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,9 @@ const {auth} = require('../middlewares/authentication')
 var router = express.Router();
 
 
-router.get('/:id',user_controller.get_user_by_id)
-router.put('/:id', user_controller.update_user_by_id)
-router.get('/:id/upcoming-classes',auth, user_controller.get_upcoming_classes)
+router.get('/:id', auth, user_controller.get_user_by_id)
+router.put('/:id', auth, user_controller.update_user_by_id)
+router.get('/:id/upcoming-classes', auth, user_controller.get_upcoming_classes)
 
 
 module.exports = router;
