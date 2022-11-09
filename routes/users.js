c6bf902655734681b9c3587f69c4a05874f73612
Ajeet <101518396+ajeet-now@users.noreply.github.com>
2022-11-09 21:34:12 +0530

    user profile pic upload api (#79)

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,6 @@ var router = express.Router();
 
 router.get('/:id',auth , user_controller.get_user_by_id)
 router.put('/:id',auth , user_controller.update_user_by_id)
+router.put('/profile-upload/:id', auth, user_controller.upload_profile_pic)
 
 module.exports = router;
