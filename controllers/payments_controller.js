8946dc056a39e7e657a59a284a0e708536cf2a07
vinayakkakkar <vinayakkakkar17@gmail.com>
2022-11-10 21:53:33 +0530

    get transactions

--- controllers/payments_controller.js
+++ controllers/payments_controller.js
@@ -18,9 +18,9 @@ exports.create_payment = async(req,resp)=>{
 exports.transaction_details = async(req,resp)=>{
         
     try{
-        var input_response =  await PaymentManager.pre_process_arena_details(req,resp)
-        var processed_reponse =  await PaymentManager.process_arena_details_input_req(input_response)
-        var post_process_response = await PaymentManager.post_arena_details_process(req,resp,processed_reponse)
+        var input_response =  await PaymentManager.pre_process_transaction_details(req,resp)
+        var processed_reponse =  await PaymentManager.process_transaction_details_input_req(input_response)
+        var post_process_response = await PaymentManager.post_transaction_details_process(req,resp,processed_reponse)
     }catch(e){
         console.log(e)
         const status_code = e.statusCode ? e.statusCode : 500
