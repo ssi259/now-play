e4ae35f9bfb69d15817bd29e632a9b86513af93f
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-11-22 15:53:07 +0530

    Implemented API to check a  eligiblity for review

--- services/review_manager.js
+++ services/review_manager.js
@@ -17,3 +17,24 @@ exports.post_review_process = async(req,resp,input_response)=>{
   resp.send(input_response)
 }
 
+exports.pre_process_check_eligibility = async(req,resp)=>{
+  const eligible = await models.Enrollment.findOne({ where: { coach_id: req.query.coach_id,user_id:req.user.user_id } });
+  if (eligible) {
+      return ({"eligibility":true});
+  }
+  else {
+      return ({"eligibility":false});
+  }
+}
+
+exports.process_check_eligibility_input_req = async(input_response)=>{
+  return input_response
+}
+
+exports.post_process_check_eligibility = async(req,resp,input_response)=>{
+  resp.send(input_response)
+}
+
+
+  
+
