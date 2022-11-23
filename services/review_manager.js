9ee2789a3705d914f3c331c4091cdba4758e1473
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-11-23 14:46:38 +0530

    update existing review for create API

--- services/review_manager.js
+++ services/review_manager.js
@@ -4,19 +4,31 @@ const Api400Error = require('../error/api400Error')
 
 
 exports.pre_process_create_review = async(req,resp)=>{
-  const result = await  models.Review.create({rating: req.body.rating,coach_id: req.body.coach_id,user_id: req.body.user_id,review_text: req.body.review_text,type: req.body.type}).then(function (review) {
-      if (review) {
-          resp.send(review);
-      } else {
-          resp.status(400).send('Error in inserting new review');
-      }
-  });
+  if(req.body.coach_id == undefined){
+    throw new Api400Error("Coach ID Not Provided")
+  }
+  return {"user_id":req.user.user_id,"coach_id":req.body.coach_id,"rating":req.body.rating,"review_text":req.body.review_text,"type":req.body.type}
 }
 exports.process_review_input_req = async(input_response)=>{
-  return input_response
+  const review_exists = await models.Review.update(
+    { rating: input_response.rating, review_text: input_response.review_text },
+    {
+      where: {
+        user_id: input_response.user_id,
+        coach_id: input_response.coach_id,
+      },
+    }
+  )
+  if (review_exists[0] == 0) {
+    return await models.Review.create(input_response);
+  }
+  else {
+    return {"message":"Review Updated"}
+  }
 }
+
 exports.post_review_process = async(req,resp,input_response)=>{
-  resp.send(input_response)
+  resp.status(200).send({ status: "success", data: input_response })
 }
 
 exports.pre_process_check_eligibility = async(req,resp)=>{
