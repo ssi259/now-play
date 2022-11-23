a7cf49e983c213630333a6ca2420de3c31531bee
dhanush-hetulabs <dhanush.kumar@hetu-labs.com>
2022-11-23 17:26:45 +0530

    update_payment_api

--- controllers/payments_controller.js
+++ controllers/payments_controller.js
@@ -17,3 +17,17 @@ exports.create_payment = async(req,resp)=>{
     }finally{
     }
 }
+
+exports.update_payment = async(req,resp)=>{
+
+    try{
+        var input_response =  await PaymentManager.pre_process_update(req,resp)
+        var processed_reponse =  await PaymentManager.process_update_input_req(req,input_response)
+        await PaymentManager.post_update_process(req,resp,processed_reponse)
+    }catch(e){
+        console.log(e)
+        const status_code = e.statusCode ? e.statusCode : 500
+        return resp.status(status_code).send({ status: "Failure", message: "payment not created" })
+    }finally{
+    }
+}
--- services/payment_manager.js
+++ services/payment_manager.js
@@ -1,6 +1,12 @@
 const models = require("../models");
 const Api500Error = require('../error/api500Error')
 
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
 
 exports.pre_process_create = async(req,resp)=>{
   var enrollment = await models.Enrollment.findOne({
@@ -29,7 +35,7 @@ exports.process_create_input_req = async(req,input_response)=>{
     await models.Payment.update({status: "failed"}, {where: {user_id: req.user.user_id,plan_id: req.body.plan_id,batch_id:req.body.batch_id}})
   } 
   
-    await models.Payment.create({plan_id: req.body.plan_id,status: "pending",user_id: req.user.user_id,coach_id: req.body.coach_id})
+    await models.Payment.create({plan_id: req.body.plan_id,status: "pending",user_id: req.user.user_id,coach_id: req.body.coach_id, price: plan.price, payment_made: "Cash"})
     await models.Enrollment.create({batch_id:req.body.batch_id,user_id: req.user.user_id, subscription_id: req.body.plan_id, status: "pending"})
   
 return {user_id: req.user.user_id , dataValues:req.body, price: plan.price}
@@ -39,6 +45,57 @@ exports.post_create_process = async(req,resp,input_response)=>{
   var formatted_response = {}
   formatted_response["status"]="success"
   formatted_response["message"]="payment request created"
+  delete input_response.dataValues.user_id
+  formatted_response["data"]=input_response
+
+  resp.status(200).send(formatted_response)
+}
+
+exports.pre_process_update = async(req,resp)=>{
+  var enrollment = await models.Enrollment.findOne({
+    where: {
+      user_id: req.user.user_id,
+      subscription_id: req.body.plan_id,
+      batch_id:req.body.batch_id
+    }
+})
+var plan = await models.SubscriptionPlan.findOne({
+  where: {
+      id: req.body.plan_id
+  }
+})
+
+var coach_resp = req.body.coach_resp
+
+return {enrollment, plan, coach_resp}
+}
+
+exports.process_update_input_req = async(req,input_response)=>{
+
+  const {if_enrolled, plan, coach_resp} = input_response;
+  plan_duration = plan.duration
+
+  if(!if_enrolled.end_date){
+    if_enrolled.end_date = new Date(if_enrolled.updatedAt)
+  } 
+
+  var new_end_date = addDays(if_enrolled.end_date, plan_duration)
+
+  if(!coach_resp) {
+    await models.Enrollment.update({status: "inactive"}, {where: {user_id: req.user.user_id,batch_id:req.body.batch_id, subscription_id: req.body.plan_id}})
+    await models.Payment.update({status: "failed"}, {where: {user_id: req.user.user_id,plan_id: req.body.plan_id,batch_id:req.body.batch_id}})
+  } else {
+    await models.Enrollment.update({status: "active", end_date:new_end_date}, {where: {user_id: req.user.user_id, batch_id:req.body.batch_id, subscription_id: req.body.plan_id}})
+    await models.Payment.update({status: "success"}, {where: {user_id: req.user.user_id,plan_id: req.body.plan_id,batch_id:req.body.batch_id}})
+  }
+  
+return {user_id: req.user.user_id , dataValues:req.body, price: plan.price}
+}
+
+exports.post_update_process = async(req,resp,input_response)=>{
+  var formatted_response = {}
+  formatted_response["status"]="success"
+  formatted_response["message"]="payment success, enrollment created successfully"
   delete input_response.dataValues.user_id
   formatted_response["data"]=input_response
 
