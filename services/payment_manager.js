137402d1f895ee94188ea4078c0606aa0fdd41a7
dhanush-hetulabs <dhanush.kumar@hetu-labs.com>
2022-11-24 11:31:25 +0530

    resolved_formatting__update_api

--- services/payment_manager.js
+++ services/payment_manager.js
@@ -1,12 +1,10 @@
 const models = require("../models");
 const Api500Error = require('../error/api500Error')
-
 function addDays(date, days) {
   var result = new Date(date);
   result.setDate(result.getDate() + days);
   return result;
 }
-
 exports.pre_process_create = async(req,resp)=>{
   var enrollment = await models.Enrollment.findOne({
     where: {
@@ -20,52 +18,39 @@ var plan = await models.SubscriptionPlan.findOne({
       id: req.body.plan_id
   }
 })
-
 return {enrollment, plan}
 }
 exports.process_create_input_req = async(req,input_response)=>{
-
   const {if_enrolled, plan} = input_response;
-
   console.log(input_response)
-
   if(if_enrolled && if_enrolled.status === 'pending') {
     await models.Enrollment.update({status: "inactive"}, {where: {user_id: req.user.user_id,batch_id:req.body.batch_id, subscription_id: req.body.plan_id}})
     await models.Payment.update({status: "failed"}, {where: {user_id: req.user.user_id,plan_id: req.body.plan_id,batch_id:req.body.batch_id}})
   } 
-  
     await models.Payment.create({plan_id: req.body.plan_id,status: "pending",user_id: req.user.user_id,coach_id: req.body.coach_id, price: plan.price, payment_mode: "Cash"})
     await models.Enrollment.create({batch_id:req.body.batch_id,user_id: req.user.user_id, subscription_id: req.body.plan_id, status: "pending"})
-  
 return {user_id: req.user.user_id , dataValues:req.body, price: plan.price}
 }
-  
 exports.post_create_process = async(req,resp,input_response)=>{
   var formatted_response = {}
   formatted_response["status"]="success"
   formatted_response["message"]="payment request created"
   delete input_response.dataValues.user_id
   formatted_response["data"]=input_response
-
   resp.status(200).send(formatted_response)
 }
-
 exports.pre_process_update = async(req,resp)=>{
-
   var payment_data = await models.Payment.findOne({
     where: {
         id: req.body.payment_id
     }
   })
-  
 var plan = await models.SubscriptionPlan.findOne({
   where: {
       id: payment_data.plan_id,
   }
 })
-
 var coach_resp = req.body.coach_resp
-
 var enrollment_data = await models.Enrollment.findOne({
   where: {
     user_id: payment_data.user_id,
@@ -75,7 +60,6 @@ var enrollment_data = await models.Enrollment.findOne({
 })
 return {payment_data, enrollment_data, plan, coach_resp}
 }
-
 exports.process_update_input_req = async(req,input_response)=>{
   const {payment_data, enrollment_data, plan, coach_resp} = input_response;
   plan_duration = plan.duration
