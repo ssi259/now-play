734364a640026e5bcfee7faaee2dae80f929559f
dhanush-hetulabs <dhanush.kumar@hetu-labs.com>
2022-11-25 11:26:23 +0530

    fixed_update_pending_req

--- services/payment_manager.js
+++ services/payment_manager.js
@@ -6,27 +6,19 @@ function addDays(date, days) {
   return result;
 }
 exports.pre_process_create = async(req,resp)=>{
-  var enrollment = await models.Enrollment.findOne({
-    where: {
-      user_id: req.user.user_id,
-      subscription_id: req.body.plan_id,
-      batch_id:req.body.batch_id
-    }
-  })
   var plan = await models.SubscriptionPlan.findOne({
     where: {
         id: req.body.plan_id
     }
   })
-  return {enrollment, plan}
+  return {plan}
 }
 exports.process_create_input_req = async(req,input_response)=>{
-  const {if_enrolled, plan} = input_response;
+  const {plan} = input_response;
   console.log(input_response)
-  if(if_enrolled && if_enrolled.status === 'pending') {
-    await models.Enrollment.update({status: "inactive"}, {where: {user_id: req.user.user_id,batch_id:req.body.batch_id, subscription_id: req.body.plan_id}})
-    await models.Payment.update({status: "failed"}, {where: {user_id: req.user.user_id,plan_id: req.body.plan_id,batch_id:req.body.batch_id}})
-  } 
+
+  await models.Enrollment.update({status: "inactive"}, {where: {user_id: req.user.user_id,batch_id:req.body.batch_id, status: "pending"}})
+  await models.Payment.update({status: "failed"}, {where: {user_id: req.user.user_id, coach_id: req.body.coach_id, status: "pending"}})
   await models.Payment.create({plan_id: req.body.plan_id,status: "pending",user_id: req.user.user_id,coach_id: req.body.coach_id, price: plan.price, payment_mode: "Cash"})
   await models.Enrollment.create({batch_id:req.body.batch_id,user_id: req.user.user_id, subscription_id: req.body.plan_id, status: "pending"})
   return {user_id: req.user.user_id , dataValues:req.body, price: plan.price}
