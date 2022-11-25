e9db1269c641a968359dc29f66936e223c4b7a85
dhanush-hetulabs <dhanush.kumar@hetu-labs.com>
2022-11-25 17:45:49 +0530

    fixed_create_payment_api

---
+++ migrations/20221125082536-add_batchid_to_payment.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    queryInterface.addColumn(
+      'Payments',
+      'batch_id',
+      {
+        type:Sequelize.INTEGER
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
