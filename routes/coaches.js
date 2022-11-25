bf696b91fc1dab4ff937d2dca36b7d0873d9b462
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-11-25 19:11:35 +0530

    route updated

--- routes/coaches.js
+++ routes/coaches.js
@@ -8,7 +8,7 @@ routes.post('/upload_images',coach_controller.uploadCoachImages);
 routes.post('/upload_documents',coach_controller.uploadCoachDocuments);
 routes.get('/', coach_controller.getCoaches)
 routes.get('/batches',auth,coach_controller.getCoachBatches)
-routes.get('/active_batches',auth,coach_controller.getCoachActiveBatches)
+routes.get('/batches/active',auth,coach_controller.getCoachActiveBatches)
 routes.get('/:id',coach_controller.getCoachById)
 routes.put('/:id', coach_controller.update_coach_by_id)
 module.exports = routes;
