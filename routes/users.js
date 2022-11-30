14e849b70b38905d0867e5217b65e2c5d16d2ebc
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-11-30 19:32:40 +0530

    auth added

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.put('/profile-upload', auth, user_controller.upload_profile_pic)
 router.get('/all', user_controller.get_all_users)
 router.get('/', auth , user_controller.get_user)
-router.put('/', user_controller.update_user) // update user on profile page of user
+router.put('/',auth, user_controller.update_user) // update user on profile page of user
 router.put('/:id', user_controller.update_user_on_adminPanel) // update user from admin panel
 
 
