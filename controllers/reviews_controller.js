6cef363086b7f0ed47f5e36f7c51c44fb322c2fe
vinayakkakkar01 <114864526+vinayakkakkar01@users.noreply.github.com>
2022-12-05 14:24:25 +0530

    vinayak/review (#160)
    * review
    
    * review_check

--- controllers/reviews_controller.js
+++ controllers/reviews_controller.js
@@ -4,7 +4,7 @@ exports.create_review = async(req,resp)=>{
         
     try{
         var input_response =  await ReviewManager.pre_process_create_review(req,resp)
-        var processed_reponse =  await ReviewManager.process_review_input_req(input_response)
+        var processed_reponse =  await ReviewManager.process_review_input_req(resp,input_response)
         var post_process_response = await ReviewManager.post_review_process(req,resp,processed_reponse)
     }catch(e){
         console.log(e)
