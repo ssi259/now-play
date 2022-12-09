37841828543325eece5fc183ec8c9ee0e7d8876d
aalesh01-hetu <117704212+aalesh01-hetu@users.noreply.github.com>
2022-12-09 17:29:36 +0530

    GET API for all enrollments (#177)
    * GET API for all enrollments
    
    * fixed bug
    
    * added player name to response
    
    * made review changes

--- controllers/enrollments_controller.js
+++ controllers/enrollments_controller.js
@@ -10,3 +10,15 @@ exports.get_enrollments = async (req, resp) => {
         return resp.status(status_code).send({ status: "failure", message: e.name})
     }
 }
+
+exports.get_all_enrollments_details = async (req, resp) => {
+    try {
+        var input_response = await enrollment_manager.pre_process_get_all_enrollments_details()
+        var processed_response = await enrollment_manager.process_get_all_enrollments_details(input_response)
+        await enrollment_manager.post_process_get_all_enrollments_details(processed_response, resp)
+    } catch (e) {
+        console.log(e)
+        const status_code = e.statusCode ? e.statusCode : 500
+        return resp.status(status_code).send({ status: "failure", message: e.name })
+    }
+}
