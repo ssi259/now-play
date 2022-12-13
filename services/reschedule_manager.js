89f4838eb9405131ad0e2cb72df44c49a39df831
vinayakkakkar01 <vinayak.kakkar@hetu-labs.com>
2022-12-13 14:24:16 +0530

    initial commit

--- services/reschedule_manager.js
+++ services/reschedule_manager.js
@@ -13,31 +13,31 @@ exports.pre_process_reschedule = async(req,resp)=>{
     if (batch_id == null) {
         return resp.status(400).send({ status: "Failure", details: "batch_id Not Provided" })
     }
-    const batch_detail = await models.findOne({
+    const batch_detail = await models.findAll({
         where: {
-            id: batch_id,
+            id: req.user.coach_id,
             status: 'active'
         }
     })
-    if (previous_end_date != batch_detail.end_date) {
-        return resp.status(400).send({ status: "Failure", details: " previous_end_date is not correct" })
+        // time of first timespan
+    var p_start_time = new Date(`${previous_start_date} ${previous_start_time} `).getTime();
+    var p_end_time = new Date(`${previous_end_date} ${previous_end_time} `).getTime();
+
+    // time of second timespan
+    var u_start_time = new Date(`${updated_start_date} ${updated_start_time} `).getTime();
+    var u_end_time = new Date(`${updated_end_date} ${updated_end_time} `).getTime();
+
+    if (Math.min(p_start_time, p_end_time) <= Math.max(u_start_time, u_end_time) && Math.max(p_start_time, p_end_time) >= Math.min(u_start_time, u_end_time)) {
+        // between
     }
-    if (previous_start_date != batch_detail.start_date) {
-        return resp.status(400).send({ status: "Failure", details: " previous_start_date is not correct" })
-    }
-    if (previous_end_time != batch_detail.end_time) {
-        return resp.status(400).send({ status: "Failure", details: " previous_end_time is not correct" })
-    }
-    if (previous_start_time != batch_detail.start_time) {
-        return resp.status(400).send({ status: "Failure", details: " previous_start_time is not correct" })
-    }
-    if (previous_days != batch_detail.days) {
-        return resp.status(400).send({ status: "Failure", details: " previous_days are not correct" })
-    }
+    for (each_batch_detail of batch_detail){
+    
+
     const rescheduled = await Reschedule.create({batch_id,previous_end_time,previous_start_time,
         updated_start_time,updated_end_time,previous_start_date,updated_start_date,
         previous_end_date,updated_end_date,previous_days,updated_days
     })
+ }
     return rescheduled
   }
   
