f93b5d3d09004c1efbe8c14c8d88c9f002f68d2e
aalesh01-hetu <aalesh.patil@hetu-labs.com>
2022-12-13 14:41:07 +0530

    saved status in DB

--- services/reschedule_manager.js
+++ services/reschedule_manager.js
@@ -5,22 +5,24 @@ const Api400Error = require('../error/api400Error')
 exports.pre_process_reschedule = async(req,resp)=>{
   }
   exports.process_reschedule_input_req = async(req,resp,input_response)=>{
-    const rescheduled = await Reschedule.create({batch_id: req.body.batch_id,previous_end_time: req.body.previous_end_time,previous_start_time: req.body.previous_start_time,
+    const batch_id = req.body.batch_id;
+      if (batch_id == null) {
+          return resp.status(400).send({ status: "Failure", details: "batch_id Not Provided" })
+      }
+    const rescheduled = await models.Reschedule.create({batch_id: req.body.batch_id,previous_end_time: req.body.previous_end_time,previous_start_time: req.body.previous_start_time,
         updated_start_time: req.body.updated_start_time,updated_end_time: req.body.updated_end_time,previous_start_date: req.body.previous_start_date,updated_start_date: req.body.updated_start_date,
         previous_end_date: req.body.previous_end_date,updated_end_date: req.body.updated_end_date
     })
-    if (batch_id == null) {
-        return resp.status(400).send({ status: "Failure", details: "batch_id Not Provided" })
+    if (updated_start_time == null, updated_end_time == null, updated_start_date == null, updated_end_date == null) {
+        await models.Reschedule.update({ status: 'cancelled' }, { where: { id: rescheduled.id } })
+        resp.status(201).send({ status: "Success", message: "Class Has Been Cancelled" })
     }
-    const batch_detail = await models.findAll({
+    const batch_detail = await models.Batches.findAll({
         where: {
             id: req.user.coach_id,
             status: 'active'
         }
     })
-    if (updated_start_time == null,updated_end_time == null,updated_start_date == null,updated_end_date == null) {
-        resp.status(201).send({status:"Success",message:"Class Has Been Cancelled"})
-    }
     for (each_batch_detail of batch_detail){
 
     // time of first timespan
