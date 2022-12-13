8297ffb13677ca7999dd4e7c94eb223ec163081a
vinayakkakkar01 <vinayak.kakkar@hetu-labs.com>
2022-12-13 14:25:18 +0530

    initial commit

--- services/reschedule_manager.js
+++ services/reschedule_manager.js
@@ -5,11 +5,10 @@ const Api400Error = require('../error/api400Error')
 exports.pre_process_reschedule = async(req,resp)=>{
   }
   exports.process_reschedule_input_req = async(req,resp,input_response)=>{
-    const {
-        batch_id,previous_end_time,previous_start_time,
-        updated_start_time,updated_end_time,previous_start_date,updated_start_date,
-        previous_end_date,updated_end_date,previous_days,updated_days} = req.body;
-    
+    const rescheduled = await Reschedule.create({batch_id: req.body.batch_id,previous_end_time: req.body.previous_end_time,previous_start_time: req.body.previous_start_time,
+        updated_start_time: req.body.updated_start_time,updated_end_time: req.body.updated_end_time,previous_start_date: req.body.previous_start_date,updated_start_date: req.body.updated_start_date,
+        previous_end_date: req.body.previous_end_date,updated_end_date: req.body.updated_end_date
+    })
     if (batch_id == null) {
         return resp.status(400).send({ status: "Failure", details: "batch_id Not Provided" })
     }
@@ -19,24 +18,22 @@ exports.pre_process_reschedule = async(req,resp)=>{
             status: 'active'
         }
     })
-        // time of first timespan
-    var p_start_time = new Date(`${previous_start_date} ${previous_start_time} `).getTime();
-    var p_end_time = new Date(`${previous_end_date} ${previous_end_time} `).getTime();
+    if (updated_start_time == null,updated_end_time == null,updated_start_date == null,updated_end_date == null) {
+        resp.status(201).send({status:"Success",message:"Class Has Been Cancelled"})
+    }
+    for (each_batch_detail of batch_detail){
+
+    // time of first timespan
+    var p_start_time = new Date(`${each_batch_detail.start_date} ${each_batch_detail.start_time} `).getTime();
+    var p_end_time = new Date(`${each_batch_detail.end_date} ${each_batch_detail.end_time} `).getTime();
 
     // time of second timespan
     var u_start_time = new Date(`${updated_start_date} ${updated_start_time} `).getTime();
     var u_end_time = new Date(`${updated_end_date} ${updated_end_time} `).getTime();
 
     if (Math.min(p_start_time, p_end_time) <= Math.max(u_start_time, u_end_time) && Math.max(p_start_time, p_end_time) >= Math.min(u_start_time, u_end_time)) {
-        // between
+        return resp.status(400).send({ status: "Failure", details: `time conflict with batch_id: ${each_batch_detail.id},please change time` })
     }
-    for (each_batch_detail of batch_detail){
-    
-
-    const rescheduled = await Reschedule.create({batch_id,previous_end_time,previous_start_time,
-        updated_start_time,updated_end_time,previous_start_date,updated_start_date,
-        previous_end_date,updated_end_date,previous_days,updated_days
-    })
  }
     return rescheduled
   }
