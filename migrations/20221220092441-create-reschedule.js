82439ae73fa2b9dd619592e3f1b0347589f9bd35
vinayakkakkar01 <114864526+vinayakkakkar01@users.noreply.github.com>
2022-12-20 19:42:11 +0530

    migration (#196)

---
+++ migrations/20221220092441-create-reschedule.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reschedules', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      batch_id: {
+        type: Sequelize.INTEGER
+      },
+      previous_end_time: {
+        type: Sequelize.TIME
+      },
+      previous_start_time: {
+        type: Sequelize.TIME
+      },
+      updated_start_time: {
+        type: Sequelize.TIME
+      },
+      updated_end_time: {
+        type: Sequelize.TIME
+      },
+      previous_start_date: {
+        type: Sequelize.DATE
+      },
+      updated_start_date: {
+        type: Sequelize.DATE
+      },
+      days: {
+        type: Sequelize.JSON
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reschedules');
+  }
+};
