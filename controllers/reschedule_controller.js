ce631d32a90eb19aa8363cd6a9acadf2aebf263f
vinayakkakkar01 <vinayak.kakkar@hetu-labs.com>
2022-12-26 12:49:14 +0530

    change of name in controller and manager

--- controllers/reschedule_controller.js
+++ controllers/reschedule_controller.js
@@ -1,6 +1,7 @@
 const rescheduleManager = require('../services/reschedule_manager');
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 exports.rescheduling = async (req, resp) => {
     try {
         const input_data = await rescheduleManager.pre_process_reschedule(req);
@@ -13,6 +14,13 @@ exports.reschedule_class = async (req, resp) => {
         const reschedule = await rescheduleManager.process_reschedule_class(input_data);
         await rescheduleManager.post_process_reschedule_class(reschedule, resp);
 >>>>>>> 2ef35be (Implemeted reschedule class)
+=======
+exports.rescheduling = async (req, resp) => {
+    try {
+        const input_data = await rescheduleManager.pre_process_reschedule(req);
+        const reschedule = await rescheduleManager.process_reschedule(input_data);
+        await rescheduleManager.post_process_reschedule(reschedule, resp);
+>>>>>>> 34570f1 (change of name in controller and manager)
     } catch (e) {
         console.log(e)
         const status_code = e.statusCode ? e.statusCode : 500
