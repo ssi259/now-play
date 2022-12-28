b1b60c12c046e00e0695a9f74d7f254e7cd64844
ajeet-now <ajeet@now.bike>
2022-12-28 17:24:06 +0530

    add otp table

---
+++ migrations/20221228111446-create-otp.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Otps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      otp: {
+        type: Sequelize.STRING
+      },
+      expiration_date: {
+        type: Sequelize.DATE
+      },
+      is_verified: {
+        type: Sequelize.BOOLEAN
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Otps');
+  }
+};
