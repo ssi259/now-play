6c131ea51559cd45a2a2ceb14395a591b3dc8fd4
vinayakkakkar01 <vinayak.kakkar@hetu-labs.com>
2022-12-29 21:36:52 +0530

    removed console log

--- services/coach_manager.js
+++ services/coach_manager.js
@@ -555,7 +555,6 @@ exports.process_get_player_details = async (input_data) => {
   if (player_details.dataValues.status === "inactive"){
     throw new Api400Error("Player is inactive")
   }
-  console.log(player_details)
   return player_details;
 
 }
