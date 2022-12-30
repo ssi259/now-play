c88592b9478ca044466d0592a3a3290874c74e9b
dhanush-hetulabs <117707833+dhanush-hetulabs@users.noreply.github.com>
2022-12-30 19:33:37 +0530

    Dhanush/send payment remainder (#225)
    * coaches can send payment remainders
    
    * Completed send payment remainder
    
    * rename_remainder
    
    * rename_remainder
    
    * rename_remainder
    
    Co-authored-by: Arjun <arjun.verma8412@gmail.com>

--- controllers/coaches_controller.js
+++ controllers/coaches_controller.js
@@ -212,3 +212,15 @@ exports.add_fcm_token = async (req, resp) => {
         return resp.status(status_code).send({ status: "failure", message: e.name,data:{}})
     }
 }
+
+exports.send_payment_reminder = async (req, resp) => {
+    try {
+        var input_response = await coachManager.pre_process_pay_reminder(req)
+        var processed_resp = await coachManager.process_pay_reminder(input_response)
+        await coachManager.post_process_pay_reminder(resp, processed_resp)
+    } catch (e) {
+        console.log(e)
+        const status_code = e.statusCode ? e.statusCode : 500
+        return resp.status(status_code).send({ status: "failure", message: e.name,data:{}})
+    }
+}
