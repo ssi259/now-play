a665b461590236473019cb88d8b7a42bf13203fb
vinayakkakkar01 <vinayak.kakkar@hetu-labs.com>
2023-01-04 13:44:09 +0530

    changes

--- services/leads_manager.js
+++ services/leads_manager.js
@@ -4,23 +4,23 @@ const Api400Error = require('../error/api400Error');
 exports.pre_process_create_lead = async (req) => {
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
-    const email = req.body.email;
+    const email_id = req.body.email_id;
     const mobile_number = req.body.mobile_number;
     const description = req.body.description;
 
-    return { first_name, last_name, email, mobile_number, description };
+    return { first_name, last_name, email_id, mobile_number, description };
 }
 
 exports.process_create_lead = async (input_data) => {
-    const { first_name, last_name, email, mobile_number, description } = input_data;
+    const { first_name, last_name, email_id, mobile_number, description } = input_data;
     if (!first_name) {
         throw new Api400Error("first_name is required");
     }
     if (!last_name) {
         throw new Api400Error("last_name is required");
     }
-    if (!email) {
-        throw new Api400Error("email is required");
+    if (!email_id) {
+        throw new Api400Error("email_id is required");
     }
     if (!mobile_number) {
         throw new Api400Error("mobile_number is required");
@@ -31,7 +31,7 @@ exports.process_create_lead = async (input_data) => {
     const lead = await models.Lead.create({
         first_name,
         last_name,
-        email,
+        email_id,
         mobile_number,
         description
     })
@@ -54,3 +54,5 @@ exports.process_get_leads = async (input_data) => {
 exports.post_process_get_leads = async (process_response, resp) => {
     return resp.status(200).send({ status: "Success", message: "Leads Fetched", data: process_response })
 }
+
+//postman data for create lead
