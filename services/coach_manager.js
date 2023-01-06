88ae8263252e908f9b506778e6722f64b8d0d181
Ajeet <101518396+ajeet-now@users.noreply.github.com>
2023-01-06 22:18:11 +0530

    fix- payment-reminder (#246)

--- services/coach_manager.js
+++ services/coach_manager.js
@@ -660,7 +660,7 @@ exports.process_pay_reminder = async (input_data) => {
     where: {id: player_id}
   })
   const payment_reminder = `${user['name']}, Your coach has requested for a payment. Please proceed according or expect a discontinuation of your sevices`
-  send_push_notifications("daOIwyOYR0uzPj88fzVf0Q:APA91bGwyLeHWfvt9E-XVfAd2jaeZz-loYTDdwlNF4gNak7QYWXSLUGaGtLEWMqVOTb5KNBwU7X7QtaTUPKxVg-VAaYBJwL7akqF5X8yUIBL9vj2lRJ-HSOUnQ_djAeGEkFYkNQOVXNA", {
+  send_push_notifications("eypvdFFQQSqKDdvVrzUwpU:APA91bHz8kUUenGSthOSU6sGD316PNJL-tQn6wQBZJnqUQu2K6Pqmhnc6xUwlzMQQNJxWf5Hdeedl1LblaOzPeYAb_eo_46nyB0inJT6ZiFxHL2LM9NCPgWPSZIKAHs2DLRI-lJwyYat", {
     title: "Payment Reminder",
     body: payment_reminder,
   })
