c7a082475b5706a7e53b9ef30c05e10d771e8c14
vinayakkakkar01 <vinayak.kakkar@hetu-labs.com>
2023-01-09 17:25:57 +0530

    .

--- utilities/send_push_notifications.js
+++ utilities/send_push_notifications.js
@@ -1,23 +1,23 @@
 
-// const admin = require("firebase-admin");
-// const serviceAccount = require("../config/rushsports-d2e5a_firebase_adminsdk.json");
+const admin = require("firebase-admin");
+const serviceAccount = require("../config/rushsports-d2e5a_firebase_adminsdk.json");
 
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
-// exports.send_push_notifications = async (tokens , notification) => {
-//     const deviceTokens = tokens;
-//     const payload = {
-//         notification: notification,
-//     };
-//     admin
-//         .messaging()
-//         .sendToDevice(deviceTokens, payload)
-//         .then((response) => {
-//             console.log(`Successfully sent message: ${response}`);
-//         })
-//         .catch((error) => {
-//             console.log(`Error sending message: ${error}`);
-//         });
-// }
+exports.send_push_notifications = async (tokens , notification) => {
+    const deviceTokens = tokens;
+    const payload = {
+        notification: notification,
+    };
+    admin
+        .messaging()
+        .sendToDevice(deviceTokens, payload)
+        .then((response) => {
+            console.log(`Successfully sent message: ${response}`);
+        })
+        .catch((error) => {
+            console.log(`Error sending message: ${error}`);
+        });
+}
