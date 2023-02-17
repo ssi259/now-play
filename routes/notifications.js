49151c800f4fee53d472eeaded2f9b9cece61793
ajeet-now <ajeet@now.bike>
2023-02-17 18:06:47 +0530

    send bulk player or coach notifications with image

--- routes/notifications.js
+++ routes/notifications.js
@@ -6,5 +6,6 @@ var router = express.Router();
 
 router.get('/', auth, notifications_controller.get_notifications)
 router.put('/', auth, notifications_controller.update_notifications)
+router.post('/', notifications_controller.send_notifications)
 
 module.exports = router
